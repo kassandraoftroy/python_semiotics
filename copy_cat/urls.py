e703d7a5190b4333339ecd9e7f6cdaf9dcf41a7b
Ari Rodriguez <superarius@gmail.com>
2017-08-08 02:23:47 -0400

    urls adjust for scaledown

--- copy_cat/urls.py
+++ copy_cat/urls.py
@@ -4,8 +4,7 @@ from . import views
 
 app_name = 'copy_cat'
 urlpatterns = [
-    url(r'^$', views.chat_home, name='chat_home'),
-    url(r'^select/$', views.select_bot, name='select'),
+    url(r'^$', views.chat_home, name='chat_home')]
+'''    url(r'^select/$', views.select_bot, name='select'),
     url(r'^start/(?P<sn_id>[0-9]+)/(?P<bot_id>[0-9]+)/$', views.start_chat, name='start'),
-    url(r'^chatroom/(?P<sn_id>[0-9]+)/(?P<bot_id>[0-9]+)/$', views.chatroom, name='chatroom')
-]
+    url(r'^chatroom/(?P<sn_id>[0-9]+)/(?P<bot_id>[0-9]+)/$', views.chatroom, name='chatroom') '''
