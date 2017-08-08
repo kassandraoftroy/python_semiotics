9b004fb1d60320787ab538f1179082915358d938
Ari Rodriguez <superarius@gmail.com>
2017-08-08 15:47:43 -0400

    spelling mistake

--- copy_cat/views.py
+++ copy_cat/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.shortcuts import render,
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils import timezone
 from .models import AI, SN, Message
